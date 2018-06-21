82133ffdeade01dac9f3845373b6ddd467db9ffd
Drakmyth <shaun.hamman@gmail.com>
2018-06-21 13:07:05 -0700

    Cleaned up comment-form javascript. Targetted comment-form redirect at anchor

--- assets/js/comment-form.js
+++ assets/js/comment-form.js
@@ -1,9 +1,13 @@
-function ToggleCommentForm() {
-    $('.slider').toggleClass('active');
-    var button = $('.comment-form>button')[0];
-    button.disabled = !button.disabled;
+function ShowCommentForm() {
+    $('.slider').addClass('active');
+    $('.comment-form>button')[0].disabled = true;
 }
 
 function CaptchaCallback(responseToken) {
     $('#submitButton')[0].disabled = false;
+}
+
+function OnSubmitComment() {
+    $('.slider').removeClass('active');
+    $('.comment-thanks').removeClass('hidden');
 }
