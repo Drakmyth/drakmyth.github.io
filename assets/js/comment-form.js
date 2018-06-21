ea69cf544d59411c482a3db2938c943708300844
Drakmyth <shaun.hamman@gmail.com>
2018-06-21 13:55:41 -0700

    Made comment post async so it doesn't redirect

--- assets/js/comment-form.js
+++ assets/js/comment-form.js
@@ -1,13 +1,35 @@
 function ShowCommentForm() {
     $('.slider').addClass('active');
     $('.comment-form>button')[0].disabled = true;
+    $("#leave-comment").submit(OnSubmitComment);
 }
 
 function CaptchaCallback(responseToken) {
     $('#submitButton')[0].disabled = false;
 }
 
-function OnSubmitComment() {
+function OnSubmitComment(event) {
+    event.preventDefault(); //prevent default action 
+    var post_url = $(this).attr("action"); //get form action url
+    var request_method = $(this).attr("method"); //get form GET/POST method
+    var form_data = $(this).serialize(); //Encode form elements for submission
+
+    var commentThanks = $('.comment-thanks');
+    
+    // TODO: Start Spinner
+
+    $.ajax({
+        url : post_url,
+        type: request_method,
+        data : form_data,
+        error: function (err) {
+            commentThanks.text('Uh oh! Something went wrong submitting your comment. Please refresh the page and try again.');
+            commentThanks.addClass('errorText');
+        }
+    }).done(function(response){
+        // TODO: End Spinner
+    });
+
     $('.slider').removeClass('active');
-    $('.comment-thanks').removeClass('hidden');
+    commentThanks.removeClass('hidden');
 }
